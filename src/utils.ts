c45a4949f792347c44c3ca37889c2cef51ef01c7
vigonotion <tom@sutomaji.net>
2023-08-15 18:18:35 +0200

    fix utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,14 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function isWin(placement: number, players: number) {
+  if (players == 5) {
+    return placement <= 2;
+  }
+
+  return placement == 1;
 }
