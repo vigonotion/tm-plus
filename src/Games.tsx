ed7f1bb181df8752ace0b089fa4709823612bdad
Tom Schneider <tom@sutomaji.net>
2023-08-15 21:54:14 +0200

    recent games

--- src/Games.tsx
+++ src/Games.tsx
@@ -8,6 +8,8 @@ import {
   TableRow,
 } from "./components/ui/table";
 import { useGames, usePlacements } from "./hooks/use-placements";
+import { Circle, Snowflake, Leaf, Trophy } from "lucide-react";
+import { isWin } from "./utils";
 
 function Placements({ game }: { game: string }) {
   const { isLoading, data } = usePlacements({
@@ -24,14 +26,30 @@ function Placements({ game }: { game: string }) {
         const plc = x.placement;
 
         return (
-          <span key={x.player}>
-            {plc === 1 ? "🥇" : plc === 2 ? "🥈" : plc === 3 ? "🥉" : "🔘"}
-            {x.expand?.player?.name}
+          <span key={x.player} className="flex items-center">
+            <span>
+              {plc}. {x.expand?.player?.name}
+            </span>
+            {isWin(plc, data.length) && (
+              <Trophy className="text-yellow-500 ml-2" size={12} />
+            )}
           </span>
         );
       })}
     </span>
   );
+}
+
+function MapIcon({ map }: { map: string }) {
+  if (map === "hellas") {
+    return <Snowflake size={16} className="text-blue-500" />;
+  }
+
+  if (map === "elysium") {
+    return <Leaf size={16} className="text-green-500" />;
+  }
+
+  return <Circle size={16} className="text-orange-500" />;
 }
 
 function Games() {
@@ -49,13 +67,22 @@ function Games() {
         <TableHeader>
           <TableRow>
             <TableHead>Game</TableHead>
+            <TableHead>Map</TableHead>
+            <TableHead>Generations</TableHead>
             <TableHead>Placements</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {data.map((game) => (
             <TableRow key={game.id}>
-              <TableCell>{game.name}</TableCell>
+              <TableCell>{game.date.split(" ")[0]}</TableCell>
+              <TableCell>
+                <div className="flex items-center gap-1">
+                  <MapIcon map={game.map} />
+                  <span className="capitalize">{game.map}</span>
+                </div>
+              </TableCell>
+              <TableCell>{game.generations}</TableCell>
               <TableCell>
                 <Placements game={game.id} />
               </TableCell>
