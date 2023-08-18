820bd359df5d6999f3d36a61a2959ba04960974f
Tom Schneider <tom@sutomaji.net>
2023-08-18 19:35:50 +0200

    tooltip

--- src/components/pages/Player.tsx
+++ src/components/pages/Player.tsx
@@ -18,6 +18,7 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import { ArrowDownLeft, ArrowDownRight, ArrowUpRight } from "lucide-react";
+import { TooltipContent } from "../ui/tooltip";
 
 function Corps({ player }: { player: string }) {
   const { data } = usePlacements({
@@ -72,6 +73,22 @@ function Corps({ player }: { player: string }) {
   );
 }
 
+const CustomTooltip = ({ active, payload, label }: any) => {
+  if (active && payload && payload.length) {
+    console.log(payload);
+
+    return (
+      <div className="z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2">
+        <div className="custom-tooltip">
+          <p className="label">{`Elo: ${payload[0].value}`}</p>
+        </div>
+      </div>
+    );
+  }
+
+  return null;
+};
+
 function EloChart({ player }: { player: string }) {
   const { ratings, isLoading } = useRatings();
 
@@ -122,7 +139,7 @@ function EloChart({ player }: { player: string }) {
                 <stop offset="95%" stopColor="#f97316" stopOpacity={0} />
               </linearGradient>
             </defs>
-            <Tooltip />
+            <Tooltip content={<CustomTooltip />} />
             <Area
               type="monotone"
               dataKey="elo"
@@ -137,8 +154,6 @@ function EloChart({ player }: { player: string }) {
       </CardContent>
     </Card>
   );
-
-  return <div>{toElo(ratings.find((x) => x.playerId == player)!.rating)}</div>;
 }
 
 export function Player({ player }: { player: string }) {
