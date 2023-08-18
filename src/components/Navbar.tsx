f8b2eecc973bccbc3344a953e6fe9c2c420c7ee6
Tom Schneider <tom@sutomaji.net>
2023-08-19 00:23:38 +0200

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,6 +10,7 @@ import {
 } from "./ui/navigation-menu";
 import { Circle } from "lucide-react";
 import { Link, useRouter, useNavigate } from "@tanstack/react-router";
+import { cn } from "@/utils";
 
 export function Navbar() {
   const router = useRouter();
@@ -37,11 +38,51 @@ export function Navbar() {
               Corporations
             </Link>
           </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link to="/elo-sim" className={navigationMenuTriggerStyle()}>
-              Elo simulator
-            </Link>
+
+          <NavigationMenuItem className="relative">
+            <NavigationMenuTrigger>Tools</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[300px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link
+                      className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        ""
+                      )}
+                      to="/elo-sim"
+                    >
+                      <div className="text-sm font-medium leading-none">
+                        Elo simulator
+                      </div>
+                      <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                        Try out how the elo algorithm works
+                      </p>
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link
+                      className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        ""
+                      )}
+                      to="/mapTool"
+                    >
+                      <div className="text-sm font-medium leading-none">
+                        Map tool
+                      </div>
+                      <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                        Generate a map svg from a map state
+                      </p>
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+              </ul>
+            </NavigationMenuContent>
           </NavigationMenuItem>
+
           <NavigationMenuItem>
             <Link to="/about" className={navigationMenuTriggerStyle()}>
               About
