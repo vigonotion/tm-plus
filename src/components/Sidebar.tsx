2754a3b70b94fb82a4809e61f889f095ef20465d
Tom Schneider <tom@sutomaji.net>
2024-02-04 10:54:12 +0100

    fake select real style

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 import { css } from "../../styled-system/css";
 import { Logo } from "@/components/Logo";
-import { ArrowUpDown, ChevronsUpDown } from "lucide-react";
-import { hstack } from "../../styled-system/patterns";
+import { ArrowUpDown, ChevronsUpDown, Hexagon, Users } from "lucide-react";
+import { hstack, vstack } from "../../styled-system/patterns";
 
 export function Sidebar() {
   return (
@@ -13,18 +13,44 @@ export function Sidebar() {
         p: "m",
       })}
     >
-      <Logo />
+      <div className={css({ ml: "4xs" })}>
+        <Logo />
+      </div>
       <div
         className={hstack({
           px: "xs",
           py: "2xs",
-          border: "1px solid token(colors.sand.6)",
+          // border: "1px solid token(colors.sand.6)",
           rounded: "s",
           justify: "space-between",
+          colorPalette: "sand",
+
+          bgColor: "colorPalette.3",
+
+          "&:hover": {
+            bgColor: "colorPalette.4",
+          },
+
+          "&:focus": {
+            bgColor: "colorPalette.5",
+          },
         })}
       >
-        <span>Tiplu</span>
-        <ChevronsUpDown className={css({ color: "sand.6" })} size={16} />
+        <div className={vstack({ alignItems: "start", gap: "4xs" })}>
+          <label
+            className={hstack({
+              fontSize: ".8em",
+              gap: "2xs",
+              color: "sand.11",
+            })}
+          >
+            <Users size={12} />
+            <span>Group</span>
+          </label>
+          <span>Tiplu</span>
+        </div>
+
+        <ChevronsUpDown className={css({ color: "sand.11" })} size={16} />
       </div>
       <div>Nav</div>
     </div>
