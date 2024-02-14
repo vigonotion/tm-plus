d45fbaa7bc8b6e26aefee20c05abfef7ff017815
Tom Schneider <tom@sutomaji.net>
2024-02-14 23:03:44 +0100

    error color -> tomato

--- src/components/atoms/FieldError.tsx
+++ src/components/atoms/FieldError.tsx
@@ -5,13 +5,14 @@ import { styled } from "@styled-system/jsx";
 
 export const FieldError = styled(AriaFieldError, {
   base: {
-    color: "ruby.12",
-    bg: "ruby.3",
+    colorPalette: "tomato",
+    color: "colorPalette.12",
+    bg: "colorPalette.3",
     fontSize: "sm",
     roundedBottom: "s",
     px: "xs",
     py: "3xs",
-    boxShadow: "0 -10px 0 token(colors.ruby.3)",
+    boxShadow: "0 -10px 0 token(colors.colorPalette.3)",
     zIndex: -1,
   },
 });
