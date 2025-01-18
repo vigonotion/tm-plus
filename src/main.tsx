0ed7328bcb0d10be539c4e92fcaf5b895c90b4ce
Tom Schneider <tom@sutomaji.net>
2025-01-18 13:27:59 +0100

    prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
-if(root) {
-    createRoot(root).render(
-        <StrictMode>
-            <App />
-        </StrictMode>,
-    )
+if (root) {
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
 }
