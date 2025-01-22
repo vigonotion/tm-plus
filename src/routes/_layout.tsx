047ba941060275a36aeba8d5643277f7eda363b9
Tom Schneider <tom@sutomaji.net>
2025-01-22 08:05:14 +0100

    panda

--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { css } from "../../styled-system/css";
 
 export const Route = createFileRoute("/_layout")({
   component: RouteComponent,
@@ -6,9 +7,34 @@ export const Route = createFileRoute("/_layout")({
 
 function RouteComponent() {
   return (
-    <div>
-      Hello "/_layout"!
-      <Outlet />
+    <div
+      data-part={"wrapper"}
+      className={css({
+        display: "grid",
+        gridTemplateColumns: "400px 1fr",
+        height: "calc(100vh - 2 * token(sizes.4))",
+        margin: "4",
+        borderRadius: "xl",
+        backgroundColor: "gray.1",
+      })}
+    >
+      <nav
+        className={css({
+          margin: "2",
+          backgroundColor: "gray.2",
+          borderRadius: "md",
+        })}
+      >
+        nav
+      </nav>
+      <aside
+        className={css({
+          //border: "1px solid token(colors.yellow.9)",
+          margin: "2",
+        })}
+      >
+        <Outlet />
+      </aside>
     </div>
   );
 }
