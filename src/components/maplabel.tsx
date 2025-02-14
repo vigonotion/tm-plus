9cd9368f313b6aa0c61bfcfe421363858f6c321f
Tom Schneider <tom@sutomaji.net>
2025-02-14 23:27:38 +0100

    maplabel

--- src/components/maplabel.tsx
+++ src/components/maplabel.tsx
@@ -7,39 +7,43 @@ import {
   RiLeafLine,
   RiSnowflakeLine,
 } from "@remixicon/react";
+import { Flex } from "@radix-ui/themes";
+
+import css from "./maplabel.module.css";
+
 function MapIcon({ map, size = 16 }: { map: string; size?: number }) {
   if (map === "hellas") {
-    return <RiSnowflakeLine size={size} />;
+    return <RiSnowflakeLine size={size} className={css.hellas} />;
   }
 
   if (map === "elysium") {
-    return <RiLeafLine size={size} />;
+    return <RiLeafLine size={size} className={css.elysium} />;
   }
 
   if (map === "utopia") {
-    return <RiFlashlightLine size={size} />;
+    return <RiFlashlightLine size={size} className={css.utopia} />;
   }
 
   if (map === "terra") {
-    return <RiEarthLine size={size} />;
+    return <RiEarthLine size={size} className={css.terra} />;
   }
 
   if (map === "vastitas") {
-    return <RiCompassLine size={size} />;
+    return <RiCompassLine size={size} className={css.vastitas} />;
   }
 
   if (map === "amazonis") {
-    return <RiHexagonLine size={size} />;
+    return <RiHexagonLine size={size} className={css.amazonis} />;
   }
 
-  return <RiCircleLine size={size} />;
+  return <RiCircleLine size={size} className={css.mars} />;
 }
 
 export function MapLabel({ map }: { map: string }) {
   return (
-    <span>
+    <Flex align={"center"} gap={"1"}>
       <MapIcon map={map} />
-      <span>{map}</span>
-    </span>
+      <span className={css.text}>{map}</span>
+    </Flex>
   );
 }
